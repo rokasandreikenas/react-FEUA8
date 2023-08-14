136ad3c5c371cf7ba13d9c6a70cff73f5bf83c4e
rokasandreikenas <shyptt@gmail.com>
2023-08-14 18:35:19 +0300

    Initialized project

---
+++ 1_paskaita/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  // returne turi buti JSX
+  // JSX - Javascript + HTML
+  return (
+    <div className="red">
+      <h1>Hello world</h1>
+    </div>
+  );
+};
+
+export default App;
