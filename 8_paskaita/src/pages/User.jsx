1865c8ffc5ac2df6e1ab3060488390f2da33bcd1
rokasandreikenas <shyptt@gmail.com>
2023-08-30 18:23:21 +0300

    Add not found example

--- 8_paskaita/src/pages/User.jsx
+++ 8_paskaita/src/pages/User.jsx
@@ -2,22 +2,33 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 const User = () => {
-  const [user, setUser] = useState(undefined);
   const { id } = useParams();
+  const [user, setUser] = useState(undefined);
+  const [isLoading, setIsloading] = useState(true);
 
   useEffect(() => {
+    setIsloading(true);
     fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
       .then((resp) => resp.json())
       .then((response) => {
-        setUser(response);
+        if (Object.keys(response).length !== 0) {
+          setUser(response);
+        }
       })
       .catch((error) => {
         console.error(error);
+      })
+      .finally(() => {
+        setIsloading(false);
       });
   }, [id]);
 
-  if (!user) {
+  if (isLoading) {
     return <div>Loading...</div>;
+  }
+
+  if (!user && !isLoading) {
+    return <div>User not found</div>;
   }
 
   return (
