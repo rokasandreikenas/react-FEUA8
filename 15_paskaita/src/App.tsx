fe057b18fd99ef1470fc6ebfa21873d8f75e7b37
rokasandreikenas <shyptt@gmail.com>
2023-09-25 19:56:49 +0300

    Add typescript examples

---
+++ 15_paskaita/src/App.tsx
@@ -0,0 +1,10 @@
+import "./primitives";
+import { locations } from "./typed-object";
+
+const App = () => {
+  const test = "";
+
+  return <div></div>;
+};
+
+export default App;
---
+++ 15_paskaita/src/primitives.ts
@@ -0,0 +1,46 @@
+/*
+TypeScript'e galioja tie patys primityvūs duomenų tipai kaip ir JavaScript'e:
+  * string - simbolių darinys
+  * number - skaičius
+  * boolean - loginis kintamasis
+  * null - tipas pasakantis, kad reikėmės/nuorodos nėra
+  * undefined - neapibrėžtas tipas
+  * symbol - skirtas unikalioms reikšmėms kurti - šio kurso metu jo nenagrinėsime
+*/
+
+// Kuriant kintamuosius, reikia šiuos tipus priskirti kintamiesiems:
+const height: number = 175.0;
+const weight: number = 78.2;
+const fullname: string = "Serbentautas";
+const surname: string = "Bordiūras";
+const age: number = 19;
+const wife: null = null;
+const innerAnimal: undefined = undefined;
+
+console.log(height, weight, fullname, surname, age, wife, innerAnimal);
+
+console.group("1. Priskirkite kintamiesiems duomenų tipus");
+{
+  const title: string = "Vermontas";
+  const subTitle: string = "Kavinė - Baras";
+  const peopleCount: number = 5;
+  const maxPeopleCount: number = 26;
+  const isOpen: boolean = true;
+  const openTime: string = "12:00";
+  const closeTime: string = "02:00";
+  const security: null = null;
+  const animal: undefined = undefined;
+
+  console.log({
+    title: [title, typeof title],
+    subTitle: [subTitle, typeof subTitle],
+    peopleCount: [peopleCount, typeof peopleCount],
+    maxPeopleCount: [maxPeopleCount, typeof maxPeopleCount],
+    isOpen: [isOpen, typeof isOpen],
+    openTime: [openTime, typeof openTime],
+    closeTime: [closeTime, typeof closeTime],
+    security: [security, typeof security],
+    animal: [animal, typeof animal],
+  });
+}
+console.groupEnd();
